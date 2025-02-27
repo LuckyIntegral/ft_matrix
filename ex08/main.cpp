c50281a09827151761211fb8790cc76f5f411346
Vitalik <frantsv2004@gmail.com>
2025-02-27 22:19:02 +0100

    add more visual tests

--- ex08/main.cpp
+++ ex08/main.cpp
@@ -8,7 +8,7 @@ int main() {
         std::cout << "Matrix trace is the sum of the diagonal elements"
                   << std::endl;
         std::cout << "Main rule: Matrix must be square" << std::endl;
-        std::cout << std::endl;
+        std::cout << "Also, tr(AB) = tr(BA)" << std::endl;
     }
     {
         Matrix<int> m1({{1, 0}, {0, 1}});
@@ -21,6 +21,12 @@ int main() {
     {
         Matrix<int> m1({{-2, -8, 4}, {1, -23, 4}, {0, 6, 4}});
         std::cout << "Test 3 result: " << m1.trace() << std::endl;
+    }
+    {
+        Matrix<int> m1({{8, 5}, {3, 4}});
+        Matrix<int> m2({{1, 2}, {9, 4}});
+        std::cout << "Test 4 result: tr(AB) = " << (m1 * m2).trace()
+                  << "; tr(BA) = " << (m2 * m1).trace() << std::endl;
     }
     return 0;
 }
